6bb44b90c9b1c217120468713f82c41907297fd8
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2019-12-17 23:00:37 +0300

    Add alt for img tags

--- src/components/EpisodePlayer/EpisodePicture.jsx
+++ src/components/EpisodePlayer/EpisodePicture.jsx
@@ -3,9 +3,14 @@ import React, { Component } from "react";
 export default class EpisodePicture extends Component {
   render() {
     if (this.props.src) {
-      return <img src={this.props.src}></img>;
+      return <img src={this.props.src} alt="The episode has no image"></img>;
     } else {
-      return <img src="https://placekitten.com/640/360"></img>;
+      return (
+        <img
+          src="https://placekitten.com/640/360"
+          alt="The episode has no image"
+        ></img>
+      );
     }
   }
 }
