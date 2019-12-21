e01b2598563e0584a162ddd8aee0853b1662b523
Ilya Kuchin <rorz.ivcyb@gmail.com>
2019-12-21 22:07:30 +0300

    Add getPodcast with hardcoded result (#4)

---
+++ src/api/GetPodcast.js
@@ -0,0 +1,12 @@
+export default function getPodcast() {
+  return {
+    name: "podcastName",
+    description: "pocastDescription",
+    image: "link",
+    episodes: [
+      { name: "episode1", description: "description1", image: "image1" },
+      { name: "episode2", description: "description2", image: "image2" },
+      { name: "episode2", description: "description2", image: "image2" }
+    ]
+  };
+}
