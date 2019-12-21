6ca00ce3b6eefd9e261a97edd04bec78897b7b53
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2019-12-21 23:10:29 +0300

    Add name, description and image to Podcast component

--- src/components/Podcast/Podcast.js
+++ src/components/Podcast/Podcast.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import getPodcast from "../../api/GetPodcast";
 
 export default class Podcast extends Component {
   render() {
-    return <div></div>;
+    return this.state ? (
+      <div>
+        <div>{this.state.name}</div>
+        <div>{this.state.description}</div>
+        <div>{this.state.image}</div>
+      </div>
+    ) : null;
+  }
+
+  componentDidMount() {
+    this.setState({ ...getPodcast() });
   }
 }
--- src/components/Podcast/Podcast.test.js
+++ src/components/Podcast/Podcast.test.js
@@ -7,6 +7,27 @@ configure({ adapter: new Adapter() });
 
 describe("Pocast component tests", () => {
   test("should render properly", () => {
-    expect(shallow(<Podcast></Podcast>).contains(<div></div>)).toBe(true);
+    const wrap = shallow(<Podcast></Podcast>);
+    wrap.setState({
+      name: "podcastName",
+      description: "pocastDescription",
+      image: "link",
+      episodes: [
+        { name: "episode1", description: "description1", image: "image1" },
+        { name: "episode2", description: "description2", image: "image2" },
+        { name: "episode2", description: "description2", image: "image2" }
+      ]
+    });
+    expect(
+      wrap.contains(
+        wrap.state ? (
+          <div>
+            <div>{wrap.state("name")}</div>
+            <div>{wrap.state("description")}</div>
+            <div>{wrap.state("image")}</div>
+          </div>
+        ) : null
+      )
+    ).toBe(true);
   });
 });
