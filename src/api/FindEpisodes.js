22393c02d8d6bae242a699c5f2d1231f087dd3bb
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2019-12-21 21:54:41 +0300

    Add findEpisodes with hardcoded result

---
+++ src/api/FindEpisodes.js
@@ -0,0 +1,7 @@
+export default function() {
+  return [
+    { name: "name1", description: "description1", image: "link1" },
+    { name: "name2", description: "description2", image: "link2" },
+    { name: "name3", description: "description3", image: "link3" }
+  ];
+}
