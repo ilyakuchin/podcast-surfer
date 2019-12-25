813b9318a3f9cd414404c7ad0743775bd29cfe95
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2019-12-25 04:26:35 +0300

    fix get request path for heroku

--- src/api/FindPodcasts.js
+++ src/api/FindPodcasts.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export default function findPodcasts(searchPhrase) {
   return axios
-    .get(`https://itunes.apple.com/search?media=podcast&term=${searchPhrase}`)
+    .get(
+      `https://podcast-web-app-dev.herokuapp.com/https://itunes.apple.com/search?media=podcast&term=${searchPhrase}`
+    )
     .then(function(response) {
       let podcastsInfo = [];
       for (let i = 0; i < response.data.results.length; i++) {
