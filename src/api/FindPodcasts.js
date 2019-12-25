2b043751e42571f481f932328b64fff66761ce02
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2019-12-25 04:06:27 +0300

    Add podcast search with itunes api

--- src/api/FindPodcasts.js
+++ src/api/FindPodcasts.js
@@ -1,7 +1,18 @@
-export default function findPodcasts() {
-  return [
-    { id: 1, name: "name1", description: "description1", image: "link1" },
-    { id: 2, name: "name2", description: "description2", image: "link2" },
-    { id: 3, name: "name3", description: "description3", image: "link3" }
-  ];
+import axios from "axios";
+
+export default function findPodcasts(searchPhrase) {
+  return axios
+    .get(`https://itunes.apple.com/search?media=podcast&term=${searchPhrase}`)
+    .then(function(response) {
+      let podcastsInfo = [];
+      for (let i = 0; i < response.data.results.length; i++) {
+        podcastsInfo.push({
+          id: response.data.results[i].collectionId,
+          name: response.data.results[i].collectionName,
+          description: response.data.results[i].collectionName,
+          image: response.data.results[i].artworkUrl600
+        });
+      }
+      return podcastsInfo;
+    });
 }
