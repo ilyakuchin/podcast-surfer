21031b81becc2e0d0722d4c82047c0f8e0c113a5
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-01-07 19:54:48 +0300

    Add css grid to podcast-player

--- src/components/EpisodePlayer/EpisodePlayer.js
+++ src/components/EpisodePlayer/EpisodePlayer.js
@@ -5,21 +5,19 @@ import "./EpisodePlayer.css";
 export default class EpisodePlayer extends Component {
   render() {
     return this.state ? (
-      <div className="container">
-        <div className="grid">
-          <img
-            className="pic"
-            width="200"
-            height="200"
-            src={this.state.pictureSrc}
-            alt=""
-          ></img>
-          <h2 className="header">{this.state.title}</h2>
-          <p className="description">{this.state.description}</p>
-          <audio className="audio" controls>
-            <source src={this.state.audioSrc} type="audio/mp4"></source>
-          </audio>
-        </div>
+      <div className="grid">
+        <img
+          className="pic"
+          width="200"
+          height="200"
+          src={this.state.pictureSrc}
+          alt=""
+        ></img>
+        <h2 className="header">{this.state.title}</h2>
+        <audio className="audio" controls>
+          <source src={this.state.audioSrc} type="audio/mp4"></source>
+        </audio>
+        <p className="description">{this.state.description}</p>
       </div>
     ) : (
       <LoadingSpinner />
@@ -33,5 +31,7 @@ export default class EpisodePlayer extends Component {
       description: this.props.location.state.description,
       audioSrc: this.props.location.state.audio.url
     });
+
+    window.scrollTo(0, 0);
   }
 }
--- src/components/Podcast/Podcast.js
+++ src/components/Podcast/Podcast.js
@@ -2,48 +2,54 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import "./Podcast.css";
 
 export default class Podcast extends Component {
   render() {
-    return this.state ? (
-      <div>
-        <div>{this.state.name}</div>
-        <div>{this.state.description}</div>
-        <img
-          width="200"
-          height="200"
-          src={this.state.image}
-          alt="podcast cover"
-        />
-        <ul>
-          {this.state.episodes.map(item => (
-            <li key={item.id}>
-              <Link
-                to={{
-                  pathname: "/episode-player",
-                  state: {
-                    name: item.name,
-                    description: item.description,
-                    image: item.image,
-                    audio: item.audio
-                  }
-                }}
-              >
-                {item.name}
-              </Link>
-              <div>{item.description}</div>
-              <img
-                width="200"
-                height="200"
-                src={item.image}
-                alt="episode cover"
-              />
-            </li>
-          ))}
-        </ul>
+    return (
+      <div className="container1">
+        {this.state ? (
+          <div className="grid1">
+            <h2 className="title1">{this.state.name}</h2>
+            <img
+              className="pic1"
+              width="200"
+              height="200"
+              src={this.state.image}
+              alt="podcast cover"
+            />
+            <p className="description">{this.state.description}</p>
+            <ul className="episodes1">
+              {this.state.episodes.map(item => (
+                <li key={item.id}>
+                  <img
+                    width="200"
+                    height="200"
+                    src={item.image}
+                    alt="episode cover"
+                  />
+                  <Link
+                    to={{
+                      pathname: "/episode-player",
+                      state: {
+                        name: item.name,
+                        description: item.description,
+                        image: item.image,
+                        audio: item.audio
+                      }
+                    }}
+                  >
+                    {item.name}
+                  </Link>
+                  {/* <div>{item.description}</div> */}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <LoadingSpinner />
+        )}
       </div>
-    ) : (
-      <LoadingSpinner />
     );
   }
 
