81afd4b3a60672a69321480d3c5fba1250a3addf
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-01-30 04:15:38 +0300

    Add login client validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { setUsername, setPassword, login } from '../../redux/actions/userInfo';
+import {
+  setUsername,
+  setPassword,
+  login,
+  setValidationErrorMessage
+} from '../../redux/actions/userInfo';
 
-export function Login({ username, password, setUsername, setPassword, login }) {
+export function Login({
+  username,
+  password,
+  setUsername,
+  setPassword,
+  login,
+  setValidationErrorMessage
+}) {
+  function validateInput() {
+    if (!username) {
+      setValidationErrorMessage('Username field is required');
+      return false;
+    }
+    if (!password) {
+      setValidationErrorMessage('Password field is required');
+      return false;
+    }
+    return true;
+  }
   return (
     <div>
       <h2>LOGIN</h2>
@@ -14,7 +37,11 @@ export function Login({ username, password, setUsername, setPassword, login }) {
         <input
           type='text'
           value={username}
-          onChange={e => setUsername(e.target.value)}
+          onChange={e => {
+            if (validateInput()) {
+              setUsername(e.target.value);
+            }
+          }}
           placeholder='username'
           name='uname'
           required
@@ -65,7 +92,9 @@ const dispatchToProps = dispatch => {
   return {
     setUsername: username => dispatch(setUsername(username)),
     setPassword: password => dispatch(setPassword(password)),
-    login: (e, username, password) => dispatch(login(e, username, password))
+    login: (e, username, password) => dispatch(login(e, username, password)),
+    setValidationErrorMessage: message =>
+      dispatch(setValidationErrorMessage(message))
   };
 };
 
