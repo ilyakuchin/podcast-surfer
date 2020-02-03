5abdd8feb7531ad5685336acd4c0b0a27550d4a7
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-02-03 05:16:13 +0300

    Add layout for signup component

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,12 +1,36 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import {
   setUsername,
   setPassword,
   signup
 } from '../../redux/actions/UserInfo/userInfo';
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+  justify-self: center;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  font-size: 18px;
+`;
+
+const Form = styled.form`
+  display: grid;
+  grid-gap: 10px;
+  align-items: center;
+  align-self: center;
+  margin-bottom: 20px;
+`;
 
 export function Signup({
   username,
@@ -19,38 +43,38 @@ export function Signup({
   const [confirmPassword, setConfirmPassword] = useState();
 
   return (
-    <div>
+    <Container>
       <h2>SIGNUP</h2>
-      <Link to='/'>Back to Login</Link>
       <div>{validationErrorMessage}</div>
-      <form>
-        <input
+      <Form>
+        <Input
           type='text'
           value={username}
           onChange={e => setUsernameConnect(e.target.value)}
           placeholder='username'
           required
         />
-        <input
+        <Input
           type='password'
           value={password}
           onChange={e => setPasswordConnect(e.target.value)}
           placeholder='password'
           required
         />
-        <input
+        <Input
           type='password'
           value={confirmPassword}
           onChange={e => setConfirmPassword(e.target.value)}
           placeholder='confirm password'
           required
         />
-        <input
+        <Input
           type='submit'
           onClick={e => signupConnect(e, username, password, confirmPassword)}
         />
-      </form>
-    </div>
+      </Form>
+      <Link to='/'>Back to Login</Link>
+    </Container>
   );
 }
 
