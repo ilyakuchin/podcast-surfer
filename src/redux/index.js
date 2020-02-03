dbcec0eaf1951439d64fecec71e75fa6a635c732
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-02-03 22:11:24 +0300

    Extract environment variable for react dev tools

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,12 +2,16 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store =
+  process.env.REACT_APP_MODE === 'dev'
+    ? createStore(
+        rootReducer,
+        compose(
+          applyMiddleware(thunk),
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+      )
+    : createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 export default store;
