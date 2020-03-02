d1c2b30a3ac52909e54fb9ebbb6aa6fd6fdb0be1
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-03-02 07:42:46 +0300

    Add pagination for episodes

--- src/components/EpisodesList/EpisodesList.jsx
+++ src/components/EpisodesList/EpisodesList.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { List, Image } from 'semantic-ui-react';
+import { List, Image, Pagination } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setCurrentEpisode } from '../../redux/actions/CurrentEpisode/currentEpisode';
@@ -18,36 +18,46 @@ const Episodes = styled.ul`
 `;
 
 export function EpisodesList({ episodes, setCurrentEpisodeConnect }) {
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <Episodes>
+      <Pagination
+        defaultActivePage={1}
+        totalPages={Math.round(episodes.length / 10)}
+        onPageChange={(e, { activePage }) => {
+          setCurrentPage(activePage);
+        }}
+      />
       <List verticalAlign='middle'>
-        {episodes.map(({ id, name, description, imageUrl, audioUrl }) => (
-          <List.Item key={id}>
-            <Image
-              style={{
-                width: '200px',
-                height: '200px',
-                objectFit: 'scale-down'
-              }}
-              src={imageUrl}
-            />
-            <List.Content>
-              <Link
-                onClick={() =>
-                  setCurrentEpisodeConnect(
-                    name,
-                    description,
-                    imageUrl,
-                    audioUrl
-                  )
-                }
-                to='/episode-player'
-              >
-                {name}
-              </Link>
-            </List.Content>
-          </List.Item>
-        ))}
+        {episodes
+          .slice((currentPage - 1) * 10, (currentPage - 1) * 10 + 10)
+          .map(({ id, name, description, imageUrl, audioUrl }) => (
+            <List.Item key={id}>
+              <Image
+                style={{
+                  width: '200px',
+                  height: '200px',
+                  objectFit: 'scale-down'
+                }}
+                src={imageUrl}
+              />
+              <List.Content>
+                <Link
+                  onClick={() =>
+                    setCurrentEpisodeConnect(
+                      name,
+                      description,
+                      imageUrl,
+                      audioUrl
+                    )
+                  }
+                  to='/episode-player'
+                >
+                  {name}
+                </Link>
+              </List.Content>
+            </List.Item>
+          ))}
       </List>
     </Episodes>
   );
